407a4a4b2fec04d8375577937fda63e3c6d7abb1
stefanoMat <stefanokaefer@gmail.com>
2020-05-19 17:32:17 -0300

    test: ensure LogControllerDecorator and controller returns are equals

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -12,7 +12,9 @@ const makeController = (): Controller => {
       const httpResponse: HttpResponse = {
         statusCode: 200,
         body: {
-          name: 'Stefano'
+          name: 'Stefano',
+          email: 'stefano@gmail.com',
+          password: '12345'
         }
       }
       return new Promise(resolve => resolve(httpResponse))
@@ -30,7 +32,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('LogController Decator', () => {
-  test('Sould call controller handle', async () => {
+  test('Should call controller handle', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
     const httpRequest = {
@@ -43,5 +45,26 @@ describe('LogController Decator', () => {
     }
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Stefano',
+        email: 'stefano@gmail.com',
+        password: '12345'
+      }
+    })
   })
 })
